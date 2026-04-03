188608e30c2b7eed747811ce1807e806d434daf5
irynafrickson <iryna@frickson.us>
2026-04-03 10:08:19 -0500

    Add files via upload

---
+++ WinFormsUnitTestApp/GreetingTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsTestApp;
+    
+    namespace WinFormsUnitTestApp
+{
+    [TestClass]
+    public sealed class GreetingTests
+    {
+        [TestMethod]
+        public void CreateGreeting_ReturnsCorrectMessage()
+        {
+            // Arrange
+            Form1 form = new Form1();
+
+            // Act
+            string result = form.CreateGreeting("Iryna");
+
+            // Assert
+            Assert.AreEqual("Hello, Iryna!", result);
+        }
+
+        [TestMethod]
+        public void CreateGreeting_ReturnsNull_WhenEmpty()
+        {
+            Form1 form = new Form1();
+
+            string result = form.CreateGreeting("");
+
+            Assert.IsNull(result);
+        }
+        public void TestMethod1()
+        {
+        }
+    }
+}
